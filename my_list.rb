8f50ac24d42304da5e0c0cf63a4a94a4a63c9c5a
Rikstone <tarikbouari@gmail.com>
2022-11-04 14:39:28 +0000

    Create Mylist class

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,15 @@
 # Create a class MyList that has an instance variable @list.
 # In MyList implement a method #each that yields successive members of 
 # @list and uses the MyEnumerable module.
+
+class MyList
+    def initialize(*list)
+      @list = []
+      list.each { |arg| @list << arg }
+    end
+  
+    def each
+      @list.each { |value| yield value if block_given? }
+    end
+  end
+  
